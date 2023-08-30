8019445a7431196e3d9620b35844ae5c86379de7
Kirill <52864442+Cleidoic@users.noreply.github.com>
2023-08-30 12:51:32 +0300

    Homework_9 done

--- Homework_9.py
+++ Homework_9.py
@@ -6,9 +6,22 @@
 import pandas as pd
 
 data = pd.read_csv('sample_data/california_housing_train.csv')
-
 filtered_data = data[(data['population'] >= 0) & (data['population'] <= 500)]
-
 average_house_value = filtered_data['median_house_value'].mean()
 
 print(f"Средняя стоимость дома, где количество людей от 0 до 500: {average_house_value}")
+
+# *********************************************************************************
+
+# Задача 2: 
+
+# Узнать какая максимальная households в зоне минимального значения population.
+
+import pandas as pd
+
+data = pd.read_csv('sample_data/california_housing_train.csv')
+min_population = data['population'].min()
+min_population_data = data[data['population'] == min_population]
+max_households = min_population_data['households'].max()
+
+print(f"Максимальное количество households в зоне с минимальным значением population: {max_households}")
